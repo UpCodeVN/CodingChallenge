604deee979addec2562872de5952b3f2f60cc395
van phuc <vanphucnguyenqn@gmail.com>
2018-01-11 00:00:52 +0700

    Learn Object in JS

---
+++ JS-01/ObjectsAndProperties.js
@@ -0,0 +1,16 @@
+//Array
+var people = {
+    name : 'Phuc',
+    lastName : 'Van',
+    age : 18,
+    job : 'developer',
+    address : '23 Wall Street'
+};
+console.log(people);
+console.log(people.name);
+console.log(people.lastName);
+
+console.log(people['age']);
+
+var xyz = 'job';
+console.log(people[xyz]);
