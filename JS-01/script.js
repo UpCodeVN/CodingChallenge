b88bbd4b099c9e06f6db49eab794fa5aaa274897
van phuc <vanphucnguyenqn@gmail.com>
2018-01-11 00:14:31 +0700

    Learn Object in JS

--- JS-01/script.js
+++ JS-01/script.js
@@ -3,19 +3,14 @@ var heightPhu = 175;
 
 var agePhuc = 23;
 var agePhu = 18;
- 
-var scorePhuc = heightPhuc + 5*agePhuc;
-var scorePhu = heightPhu + 5*agePhu;
 
-if (scorePhuc > scorePhu)
-{
-    console.log('Phuc wins the games with ' + scorePhuc+'points!');
-}
-else if (scorePhu > scorePhuc)
-{
-    console.log('Phu wins the games with ' + scorePhu+'points!');
-}
-else if (scorePhu == scorePhuc)
-{
+var scorePhuc = heightPhuc + 5 * agePhuc;
+var scorePhu = heightPhu + 5 * agePhu;
+
+if (scorePhuc > scorePhu) {
+    console.log('Phuc wins the games with ' + scorePhuc + 'points!');
+} else if (scorePhu > scorePhuc) {
+    console.log('Phu wins the games with ' + scorePhu + 'points!');
+} else if (scorePhu == scorePhuc) {
     console.log('There is a draw');
 }
